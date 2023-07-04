0192ea4f30ae7d38ffb1b8594780ac7bda2d7765
Peter Conenna <pconenna@depaul.edu>
2023-07-03 19:21:52 -0500

    started models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,35 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category,{
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+})
 // Categories have many Products
-
+Category.hasMany(Product,{
+  // not sure if this will work
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE'
+})
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag,{
+  foreignKey: 'tag_id',
+  through:{
+    model: ProductTag,
+    unique:false
+  },
+  as: 'product_tag'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{
+  foreignKey: 'product_id',
+  through:{
+    model: ProductTag,
+    unique:false
+  },
+  as: 'tag_name'
+})
 
 module.exports = {
   Product,
