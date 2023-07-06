f1bd513fe97171eaf88ddfa1500ea325a42d1d02
Peter Conenna <pconenna@depaul.edu>
2023-07-06 00:02:33 -0500

    category routes

--- models/index.js
+++ models/index.js
@@ -6,12 +6,12 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category,{
-  foreignKey: 'category_id'
+  foreignKey: 'category_id',
 })
 // Categories have many Products
 Category.hasMany(Product,{
   // not sure if this will work
-  foreignKey: 'category_id'
+  foreignKey: 'category_id',
 })
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag,{
